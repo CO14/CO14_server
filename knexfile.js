37408c825e9a43729130bcaa234847994290b6c2
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-07-03 10:58:32 -0600

    Added Knex

---
+++ knexfile.js
@@ -0,0 +1,15 @@
+// Update with your config settings.
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  development: {
+    client: 'postgresql',
+    connection: 'postgres://localhost/14er'
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL + '?ssl=true'
+  }
+};
