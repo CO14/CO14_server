cb5e3f12b0c57935967909a37d312314c8af8faa
JAKEFELDMAN <jacob.h.feldman@gmail.com>
2017-07-06 17:14:32 -0600

    Updated Users Query

--- api/users.js
+++ api/users.js
@@ -12,9 +12,39 @@ function isValidId(req, res, next) {
 }
 
 router.get('/:id', isValidId, (req, res, next) => {
-  queries.getUserByID(req.params.id)
-    .then(user => {
-      res.json(user);
+  queries.getUserProfilePeak(req.params.id)
+    .then(accounts => {
+      const userProfile = [];
+      const userCollection = {};
+      accounts.forEach(acct => {
+        if (!userCollection[acct.account_id]) {
+          const userInstance = {
+            account_id: acct.account_id,
+            first_name: acct.first_name,
+            last_name: acct.last_name,
+            email: acct.email,
+            profile_image: acct.image,
+            facebook_url: acct.facebook_url,
+            instagram_url: acct.instagram_url,
+            twitter_url: acct.twitter_url,
+            peak: []
+          }
+          userProfile.push(userInstance);
+          userCollection[acct.account_id] = userInstance;
+        }
+        userCollection[acct.account_id].peak.push({
+          peak_id: acct.peak_id,
+          peak_name: acct.peak_name,
+          elevation: acct.elevation,
+          difficulty: acct.difficulty,
+          peak_image_url: acct.image_url,
+          account_rating: acct.account_rating,
+          account_image_url: acct.account_image_url,
+          range_id: acct.range_id,
+          range_name: acct.name
+        });
+      });
+      res.json(userProfile)
     });
 });
 
--- db/queries.js
+++ db/queries.js
@@ -1,7 +1,13 @@
 const knex = require('./knex');
 
 module.exports = {
-  getUserByID(id) {
-    return knex.select("*").from('account').where('id', id).first();
+  getUserByID: id => {
+    return knex('account').where('id', id).first();
+  },
+  getUserProfilePeak: id => {
+    return knex.select('*').from('account').where('account.id', id)
+      .join('account_peak', 'account.id', 'account_id')
+      .join('peak', 'peak_id', 'peak.id')
+      .join('range', 'range_id', 'range.id')
   }
 };
